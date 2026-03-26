ebb41c3e697700fe4333c7d4ca04c0cfbc97bc79
mrad-beep <143346323+mrad-beep@users.noreply.github.com>
2026-03-27 00:48:41 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,19 @@
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "7055430678",
+database: "form_builder"
+});
+
+db.connect((err) => {
+if(err){
+ console.log("Database connection failed",err);
+}else{
+ console.log("MySQL Connected");
+}
+});
+
+module.exports = db;
